0052a084723d7084d50997ef295454b528e7cb7b
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-19 19:18:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailAddressParser
   attr_accessor :email_addresses
 
   def parse(email_addresses)
-    @email_addresses = email_addresses.split(/[,\s]/)
+    @email_addresses = email_addresses.split(/[, ]/).delete_if { |add| add == "" }
     @email_addresses
   end
 
